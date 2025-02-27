729566cb00b4042511c0bea2dbdcdb55622559ab
jsan2321 <jsantiago.c@hotmail.com>
2025-02-27 14:49:38 -0500

    create user entity

---
+++ src/main/java/com/ecoapi/goodshopping/model/User.java
@@ -0,0 +1,24 @@
+package com.ecoapi.goodshopping.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @NaturalId
+    private String email;
+    private String password;
+}
